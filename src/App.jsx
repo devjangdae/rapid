4c6586c13e95ecb12d47b794ec027fe90fadce8a
devjangdae <tjqkd0912@gmail.com>
2023-02-05 23:04:34 +0900

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h1>Rapid START!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</h1>
+      <h1>Rapid START!!!!!!!!!!!!!!!!!!!!!!!!test commit!!!!!!!!!!!!!!!!!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
